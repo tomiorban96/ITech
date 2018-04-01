5cb08e6313563a5e4236677ceae61b38cfe20aff
tomiorban96 <tomiorban96@gmail.com>
2018-04-01 19:57:03 +0200

    Lamp utolsó verzió

--- Lamp/main.cpp
+++ Lamp/main.cpp
@@ -20,7 +20,7 @@ struct Field {
 	Type type;
 	int min;
 	int current;
-	int lamp;
+	int lamp;		//0 = Nincs; 3, 5, 10 = Hagyományos; 11 = Spot
 
 	Field() : current(0), lamp(0) {}
 };
@@ -42,12 +42,102 @@ Field **grid;
 void Debug() {
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < K; j++) {
-			if (grid[i][j].type == Wall) cout << "F";
-			if (grid[i][j].type == Glass) cout << "U";
-			if (grid[i][j].type == Empty) cout << grid[i][j].min;
+			cout << grid[i][j].current << " ";
 		}
 		cout << endl;
 	}
+}
+
+bool putLamp(int x, int y, char type, int l) {
+
+	if (type == 'S') {
+		if (grid[x][y].lamp == 0) {
+			grid[x][y].lamp = 11;
+			grid[x][y].current = 10;
+			return true;
+		}
+		return false;
+	}
+
+	int ray = l - 1;
+
+	if (grid[x][y].current != 0 || grid[x][y].type == Wall || grid[x][y].type == Glass) return false;
+
+	for (int i=x+1;i<K;i++) {
+		if (ray==0) break;
+		if (grid[y][i].type == Wall) break;
+		else if (grid[y][i].type == Glass) ray -= 2;
+		else if (grid[y][i].lamp != 0 && grid[y][i].lamp != 11) return false;
+		ray--;
+	} 
+
+	ray = l - 1;
+	for (int i=x-1;i>-1;i--) {
+		if (ray==0) break;
+		if (grid[y][i].type == Wall) break;
+		else if (grid[y][i].type == Glass) ray -= 2;
+		else if (grid[y][i].lamp != 0 && grid[y][i].lamp != 11) return false;
+		ray--;
+	}
+
+	ray = l - 1;
+	for (int i=y+1;i<N;i++) {
+		if (ray==0) break;
+		if (grid[i][x].type == Wall) break;
+		else if (grid[i][x].type == Glass) ray -= 2;
+		else if (grid[i][x].lamp != 0 && grid[i][x].lamp != 11) return false;
+		ray--;
+	}
+
+	ray = l - 1;
+	for (int i=y-1;i>-1;i--) {
+		if (ray==0) break;
+		if (grid[i][x].type == Wall) break;
+		else if (grid[i][x].type == Glass) ray -= 2;
+		else if (grid[i][x].lamp != 0 && grid[i][x].lamp != 11) return false;
+		ray--;
+	}
+
+	grid[y][x].current = l;
+	grid[y][x].lamp = l;
+
+	ray = l - 1;
+	for (int i=x+1;i<K;i++) {
+		if (ray<=0) break;
+		if (grid[y][i].type == Wall) break;
+		else if (grid[y][i].type == Glass) ray -= 2;
+		else grid[y][i].current += ray;
+		ray--;
+	} 
+
+	ray = l - 1;
+	for (int i=x-1;i>-1;i--) {
+		if (ray<=0) break;
+		if (grid[y][i].type == Wall) break;
+		else if (grid[y][i].type == Glass) ray -= 2;
+		else grid[y][i].current += ray;
+		ray--;
+	}
+
+	ray = l - 1;
+	for (int i=y+1;i<N;i++) {
+		if (ray<=0) break;
+		if (grid[i][x].type == Wall) break;
+		else if (grid[i][x].type == Glass) ray -= 2;
+		else grid[i][x].current += ray;
+		ray--;
+	}
+
+	ray = l - 1;
+	for (int i=y-1;i>-1;i--) {
+		if (ray<=0) break;
+		if (grid[i][x].type == Wall) break;
+		else if (grid[i][x].type == Glass) ray -= 2;
+		else grid[i][x].current += ray;
+		ray--;
+	}
+
+	return true;
 }
 
 int main(){
@@ -83,24 +173,15 @@ int main(){
 
 	list<Lamp> lampList;
 
-	for (int i = 0; i < N; i++) {
+	/*for (int i = 0; i < N; i++) {
 		for (int j = 0; j < K; j++) {
 			if (grid[i][j].type == Empty) {
 				int min = grid[i][j].min;
 				if (min > 0) {
-					lampList.push_back(Lamp(j, i, 'S'));
-					/*if (min <= 3) {
-						grid[i][j].current = 3;
-						lampList.push_back(Lamp(j, i, 'A'));
-					}
-					else if (min <= 5) {
-						grid[i][j].current = 5;
-						lampList.push_back(Lamp(j, i, 'B'));
-					}
-					else {
-						grid[i][j].current = 10;
-						lampList.push_back(Lamp(j, i, 'C'));
-					}*/
+					//lampList.push_back(Lamp(j, i, 'S'));
+					if (putLamp(i, j, 'S', 11))
+						lampList.push_back(Lamp(j, i, 'S'));
+
 				}
 			}
 
@@ -111,8 +192,12 @@ int main(){
 
 	for (auto a : lampList) {
 		cout << a.x << " " << a.y << " " << a.type << endl;
-	}
-
+	}*/
+	cout << endl;
+	putLamp(0, 0, 'C', 10);
+	putLamp(3, 3, 'C', 10);
+	putLamp(0, 4, 'C', 10);
+	Debug();
 
 
 	for (int i = 0; i < N; i++) {
