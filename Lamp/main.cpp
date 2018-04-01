8847bbf5f02adc1d91a12c4d1012ed7e0848df96
tomiorban96 <tomiorban96@gmail.com>
2018-04-01 13:54:52 +0200

    Világítás alap

--- Lamp/main.cpp
+++ Lamp/main.cpp
@@ -1,106 +1,124 @@
 
 #include <stdio.h>
 #include <iostream>
-int N, M;
+#include <list>
+#include <iterator>
+
+
+int N, K, M;
 using namespace std;
 
-enum Lamp {
-	A,
-	B,
-	C,
-	S
+
+
+enum Type {
+	Empty,
+	Wall,
+	Glass
 };
 
-class Field {
-public:
-	virtual int Pass(int L) = 0;
+struct Field {
+	Type type;
+	int min;
+	int current;
+	int lamp;
+
+	Field() : current(0), lamp(0) {}
 };
 
-class Empty : public Field {
-public:
+struct Lamp {
+	int x;
+	int y;
+	char type;
 
-	int lumination;
-	int required;
-	Lamp lamp;
-
-	Empty() {
-		lumination = 0;
-		required = 0;
-	}
-
-	Empty (int r) {
-		required = r;
-	}
-
-	int Pass(int L) {
-		return L - 1;
+	Lamp(int _x, int _y, char c) {
+		x = _x + 1;
+		y = _y + 1;
+		type = c;
 	}
 };
 
-class Wall : public Field {
-public:
-	int Pass(int L) {
-		return 0;
+Field **grid;
+
+void Debug() {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < K; j++) {
+			if (grid[i][j].type == Wall) cout << "F";
+			if (grid[i][j].type == Glass) cout << "U";
+			if (grid[i][j].type == Empty) cout << grid[i][j].min;
+		}
+		cout << endl;
 	}
-};
-
-class Glass : public Field {
-public:
-	int Pass(int L) {
-		return L - 3;
-	}
-};
-
-class Grid {
-public:
-	static Field grid[][];
-
-	static void read() {
-		cin >> N >> K;
-		grid = new Field[N][K];
-	}
-};
-
-int getMaxLight(int x, int y) {
-
-	int sum = 0;
-	
-	//Jobbra
-	int light = 9;
-	for (int i = x + 1; i < K; i++) {
-		sum += light;
-		light--;
-	}
-
-	//Balra
-	light = 9;
-	for (int i = x - 1; i >= 0; i--) {
-		sum += light;
-		light--;
-	}
-
-	//Lefelé
-	light = 9;
-	for (int i = y - 1; i >= 0; i--) {
-		sum += light;
-		light--;
-	}
-
-	//Felfelé
-	light = 9;
-	for (int i = y + 1; i < N; i++) {
-		sum += light;
-		light--;
-	}
-
-	return sum;
 }
 
 int main(){
 
-	//Field f1;
-	Wall w;
-	Empty e;
-	Glass g;
+	cin >> N >> K;
+
+	//grid = new Field[N][K];
+	grid = new Field*[N];
+	for (int i = 0; i < N; i++) {
+		grid[i] = new Field[K];
+	}
+
+
+	//Beolvasás
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < K; j++) {
+			char c;
+			cin >> c;
+			if (c < 58) {
+				grid[i][j].type = Empty;
+				grid[i][j].min = (int)c - (int)'0';
+			}
+			else if (c == 'F') {
+				grid[i][j].type = Wall;
+			}
+			else {
+				grid[i][j].type = Glass;
+			}
+		}
+	}
+
+	//Debug();
+
+	list<Lamp> lampList;
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < K; j++) {
+			if (grid[i][j].type == Empty) {
+				int min = grid[i][j].min;
+				if (min > 0) {
+					lampList.push_back(Lamp(j, i, 'S'));
+					/*if (min <= 3) {
+						grid[i][j].current = 3;
+						lampList.push_back(Lamp(j, i, 'A'));
+					}
+					else if (min <= 5) {
+						grid[i][j].current = 5;
+						lampList.push_back(Lamp(j, i, 'B'));
+					}
+					else {
+						grid[i][j].current = 10;
+						lampList.push_back(Lamp(j, i, 'C'));
+					}*/
+				}
+			}
+
+		}
+	}
+
+	cout << lampList.size() << endl;
+
+	for (auto a : lampList) {
+		cout << a.x << " " << a.y << " " << a.type << endl;
+	}
+
+
+
+	for (int i = 0; i < N; i++) {
+		delete[] grid[i];
+	}
+	delete[] grid;
+	
 	return 0;
 }
